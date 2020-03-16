246b798e584842b520e9d65f3c518f8387fd5c63
ChienChienouo <59683224+ChienChienouo@users.noreply.github.com>
2020-03-16 17:35:05 +0800

    修改了一下lottery.js的程式碼，剛剛的還是會跑重複的數字

--- lottery.js
+++ lottery.js
@@ -1,17 +1,18 @@
 document.querySelector(".lottery__button").addEventListener("click",function(e){
     var lottery = new Array();
     for(let i =0;i<7;i++){
-        let rd = getRandom();
-        for(let j = 0;j<7;j++){
-            while(lottery[j] == rd){
-                rd = getRandom();
+        lottery[i] = getRandom();
+        for(let j = 0;j < i;j++){
+            while(lottery[j] == lottery[i]){
+                j = 0;
+                lottery[i] = getRandom();
             }
         }
-        lottery[i] = rd;
     }
     for(let i=0;i<7;i++){
         document.getElementsByClassName("lottery__num")[i].value = lottery[i];
     }
+    console.log(lottery);
 })
 function getRandom(){
     return Math.floor(Math.random()*52)+1;
