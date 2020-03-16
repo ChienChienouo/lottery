fb870c7d2f0c344180221970b90022f5fc180db1
ChienChienouo <59683224+ChienChienouo@users.noreply.github.com>
2020-03-16 14:30:29 +0800

    寫入樂透程式碼

--- lottery.js
+++ lottery.js
@@ -1,3 +1,23 @@
-lottery(){
-    
+document.querySelector(".lottery__button").addEventListener("click",function(e){
+    var lottery = [];
+    for(let i =0;i<7;i++){
+        let rd = getRandom();
+        if(lottery.indexOf(rd)>0){
+            i -= 1;
+        }
+        else{
+            lottery.push(rd);
+        }
+    }
+    show(lottery);
+})
+
+function getRandom(){
+    return Math.floor(Math.random()*52)+1;
+}
+
+function show(lottery){
+    for(let i=0;i<7;i++){
+        document.getElementsByClassName("number")[i].value = lottery[i];
+    }
 }
